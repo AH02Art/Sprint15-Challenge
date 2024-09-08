bb53f7e84455eedb4e6f4db7381b24a5eb39b22d
Alex Handley <smithvillescholarsah@gmail.com>
2024-09-08 15:14:47 -0500

    tests set up

--- api/server.test.js
+++ api/server.test.js
@@ -6,16 +6,16 @@ test('sanity', () => {
 })
 
 describe("[POST] /api/auth/register", () => {
-  test.todo("[1]test goes here");
-  test.todo("[2]test goes here");
+  test.todo("[1] responds the correct error message if username is already taken");
+  test.todo("[2] resolves the newly registered user and their hashed password");
 })
 
 describe("[POST] /api/auth/login", () => {
-  test.todo("[3]test goes here");
-  test.todo("[4]test goes here");
+  test.todo("[4] responds the correct error message if username or password are entered incorrectly");
+  test.todo("[5] resolves the logged in user and their authorization token");
 })
 
 describe("[GET] /api/jokes", () => {
-  test.todo("[5]test goes here");
-  test.todo("[6]test goes here");
+  test.todo("[5] responds the correct error message if there's no token or it's invalid");
+  test.todo("[6] resolves with the data by valid authorization token");
 })
